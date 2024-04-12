39212039162817cd1af865757e1b2ae8f2a0bd94
TobiramaSenjuu <gmsanimations@gmail.com>
2024-04-12 16:13:08 +0300

    utf-8 ver

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-# -*- coding: utf-8 -*-
-
-
 import geometry
 
 area_of_triangle = geometry.triangle_area(10, 5, 7)
 
 area_of_circle = geometry.circle_area(13)
 
-print(f"Площадь треугольника равна: {area_of_triangle}")
-print(f"Площадь круга равна: {area_of_circle}")
+print(f"РџР»РѕС‰Р°РґСЊ С‚СЂРµСѓРіРѕР»СЊРЅРёРєР° СЂР°РІРЅР°: {area_of_triangle}")
+print(f"РџР»РѕС‰Р°РґСЊ РєСЂСѓРіР° СЂР°РІРЅР°: {area_of_circle}")
